74d5be1717d017cca7b4b4af72909c557945904f
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-01 12:48:45 -0800

    Move utils to separate file

--- src/etl.py
+++ src/etl.py
@@ -1,9 +1,8 @@
-
 import luigi
 from luigi import configuration
 import subprocess
 import os.path
-import hashlib
+import src.utils.pipeline_funs as pf
 
 import logging
 import logging.config
@@ -12,47 +11,6 @@ logging.config.fileConfig(logging_conf)
 logger = logging.getLogger("microbiome.pred")
 
 
-# helper-functions-------------------------------------------------------------
-def hash_name(long_name, length=15):
-    long_name = long_name.encode("utf-8")
-    x = int(hashlib.sha1(long_name).hexdigest(), 16) % (10 ** length)
-    return str(x)
-
-
-def processed_data_dir(project_dir, output_name):
-    """
-    Full path to melted counts output
-
-    Args
-    ----
-    project_dir [str]: Path to the main project directory.
-    output_name [str]: The name to save the file as.
-
-    Returns
-    -------
-    output [str]: The path obtained by concatenating the project directory with
-      the path to the processed data subdirectory.
-    """
-    return os.path.join(project_dir, "data", "processed", output_name)
-
-
-def rscript_dir(project_dir):
-    """
-    Full path to directory containing Rscripts
-
-    Args
-    ----
-    project_dir [str]: Path to the main project directory.
-
-    Returns
-    -------
-    output [str]: The path obtained by concatenating the project directory with
-      the path to the subdirectory containing RScripts
-    """
-    return os.path.join(project_dir, "src", "Rscripts")
-
-
-# luigi-tasks -----------------------------------------------------------------
 class PhyloseqExists(luigi.ExternalTask):
     """
     Check whether the specified phyloseq object exists
@@ -82,12 +40,16 @@ class PreprocessCounts(luigi.Task):
         return_code = subprocess.call(
             [
                 "Rscript",
-                os.path.join(rscript_dir(project_dir), "preprocess_counts.R"),
+                os.path.join(
+                    pf.rscript_dir(project_dir),
+                    "preprocess_counts.R"
+                ),
                 self.ps_path,
                 os.path.join(project_dir, self.preprocess_conf),
-                processed_data_dir(
+                pf.processed_data_dir(
                     project_dir,
-                    "preprocessed_" + hash_name(self.preprocess_conf) +".RDS"
+                    "preprocessed_" +
+                    pf.hash_name(self.preprocess_conf) + ".RDS"
                 )
             ]
 
@@ -97,9 +59,10 @@ class PreprocessCounts(luigi.Task):
 
     def output(self):
         project_dir = self.conf.get("paths", "project_dir")
-        output_name = processed_data_dir(
+        output_name = pf.processed_data_dir(
            project_dir,
-           "preprocessed_" + hash_name(self.preprocess_conf) +".RDS"
+           "preprocessed_" +
+            pf.hash_name(self.preprocess_conf) + ".RDS"
         )
         return luigi.LocalTarget(output_name)
 
@@ -121,24 +84,30 @@ class MeltCounts(luigi.Task):
 
     def run(self):
         project_dir = self.conf.get("paths", "project_dir")
+        output_name = pf.processed_data_dir(
+            project_dir,
+            "melted_" +
+            pf.hash_name(self.preprocess_conf) + ".feather"
+        )
         return_code = subprocess.call(
             [
                 "Rscript",
-                os.path.join(rscript_dir(project_dir), "melt_counts.R"),
+                os.path.join(
+                    pf.rscript_dir(project_dir), "melt_counts.R"
+                ),
+                output_name,
                 self.input().open("r").name,
-                processed_data_dir(
-                    project_dir,
-                    "melted_" + hash_name(self.preprocess_conf) +".feather"
-                )
             ]
         )
+
         if return_code != 0:
             raise ValueError("melt_counts.R failed")
 
     def output(self):
         project_dir = self.conf.get("paths", "project_dir")
-        output_name = processed_data_dir(
+        output_name = pf.processed_data_dir(
             project_dir,
-            "melted_" + hash_name(self.preprocess_conf) +".feather"
+            "melted_" +
+            pf.hash_name(self.preprocess_conf) + ".feather"
         )
         return luigi.LocalTarget(output_name)
