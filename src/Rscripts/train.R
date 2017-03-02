4076b4823f5297843f54c774aa99599346ab3e10
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-02 13:45:57 -0800

    Example training script

---
+++ src/Rscripts/train.R
@@ -0,0 +1,44 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## R script for training models on featurized data. We assume cv has been
+## considered elsewhere.
+
+## ---- arguments ----
+args <- commandArgs(trailingOnly = TRUE)
+
+message("Executing train.R with arguments:")
+message(paste(args, collapse = "\n"))
+
+x_path <- args[[1]]
+y_path <- args[[2]]
+output_path <- args[[3]]
+model_conf <- args[[4]]
+
+## ---- libraries ----
+library("caret")
+library("dplyr")
+library("feather")
+library("jsonlite")
+
+## ---- train-model ----
+x <- read_feather(x_path)
+y <- read_feather(y_path)
+model_opts <- read_json(model_conf)
+
+stopifnot(x$Meas_ID == y$Meas_ID)
+stopifnot(x$rsv == y$rsv)
+
+x <- x %>%
+  select(-Meas_ID, -rsv) %>%
+  as.matrix()
+
+y <- y %>%
+  select(count) %>%
+  unlist() %>%
+  as.numeric()
+
+model_res <- do.call(train, c(list("x" = x, "y" = y), model_opts))
+
+## ---- save-result ----
+save(model_res, file = output_path)
