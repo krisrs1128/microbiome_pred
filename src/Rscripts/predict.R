4859ed1f8e9b37081e96df997e64d3654afc2931
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-02 14:38:25 -0800

    Script to make and save predictions

---
+++ src/Rscripts/predict.R
@@ -0,0 +1,35 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## R script for training models on featurized data. We assume cv has been
+## considered elsewhere.
+
+## ---- arguments ----
+args <- commandArgs(trailingOnly = TRUE)
+
+message("Executing predict.R with arguments:")
+message(paste(args, collapse = "\n"))
+
+x_path <- args[[1]]
+model_path <- args[[2]]
+output_path <- args[[3]]
+
+## ---- libraries ----
+library("caret")
+library("dplyr")
+library("feather")
+
+## ---- train-model ----
+x <- read_feather(x_path)
+model <- get(load(model_path))
+
+x <- x %>%
+  select(-Meas_ID, -rsv) %>%
+  as.matrix()
+
+y_hat <- predict(model) %>%
+  tbl_df() %>%
+  rename(y_hat = value)
+
+## ---- save-result ----
+write_feather(y_hat, path = output_path)
