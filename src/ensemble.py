7b48849fd16d499d51eea5db7a1992b15cd5d187
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-03 17:33:21 -0800

    Sketch run and output methods for ensembling

--- src/ensemble.py
+++ src/ensemble.py
@@ -2,7 +2,9 @@ import luigi
 from luigi import configuration
 import os.path
 import json
-from src.cv_eval import CVEval
+import subprocess
+import src.utils.pipeline_funs as pf
+from src.predict import Predict
 
 import logging
 import logging.config
@@ -30,6 +32,8 @@ class Ensemble(luigi.Task):
 
     def requires(self):
         ensemble = values_from_conf(self.conf, "ensemble")
+        ensemble = ensemble[self.ensemble_id]
+
         exper = values_from_conf(self.conf, "experiment")
         ps_path = os.path.join(
             self.conf.get("paths", "project_dir"),
@@ -56,3 +60,67 @@ class Ensemble(luigi.Task):
                 )
 
         return tasks
+
+    def run(self):
+        ensemble = values_from_conf(self.conf, "ensemble")
+        ensemble = ensemble[self.ensemble_id]
+
+        exper = values_from_conf(self.conf, "experiment")
+        preds_basenames = ""
+        models_basenames = ""
+
+
+        # get paths to experiment results we needs in ensembling
+        for i in exper.keys():
+            if i not in ensemble["exper_ids"]:
+                continue
+
+            specifiers_list = [
+                exper[i]["preprocessing"],
+                exper[i]["validation_prop"],
+                exper[i]["k_folds"],
+                exper[i]["features"],
+                exper[i]["model"]
+            ]
+
+            preds_basenames +=  ";" + pf.output_name(
+                self.conf, specifiers_list, "preds_"
+            )
+            models_basenames +=";" + pf.output_name(
+                self.conf, specifiers_list, "model_"
+            )
+
+            # These are assumed constant over experiments, so safe to overwrite
+            y_basename = pf.output_name(self.conf, specifiers_list[:3])
+            k_folds = exper[i]["k_folds"]
+            new_data_path = pf.output_name(
+                self.conf, specifiers_list[:4], "features_",
+            ) + "-all.feather"
+
+        output_path = pf.output_name(
+            self.conf, self.ensemble_id, "test-all.feather"
+        )
+
+        # Now call the ensemble script
+        return_code = subprocess.call(
+            [
+                "Rscript",
+                pf.rscript_file(self.conf, "ensemble.R"),
+                preds_basenames,
+                models_basenames,
+                y_basename,
+                k_folds,
+                new_data_path,
+                output_path,
+                self.ensemble
+            ]
+        )
+
+        if return_code != 0:
+            raise ValueError("predict.R failed")
+
+    def output(self):
+        output_path = pf.output_name(
+            self.conf, self.ensemble_id, "test-all.feather"
+        )
+        return luigi.LocalTarget(output_path)
