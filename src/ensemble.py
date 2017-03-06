1cd493d6a1fb73f7c76214879227da2977774eed
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-05 16:36:59 -0800

    Task now refers to output models, not predictions

--- src/ensemble.py
+++ src/ensemble.py
@@ -68,7 +68,7 @@ class Ensemble(luigi.Task):
                 exper[i]["model"]
             ]
 
-            preds_basenames +=  pf.output_name(
+            preds_basenames += pf.output_name(
                 self.conf, specifiers_list, "preds_"
             ) + ";"
             models_basenames += pf.output_name(
@@ -76,15 +76,14 @@ class Ensemble(luigi.Task):
             ) + ";"
 
             # These are assumed constant over experiments, so safe to overwrite
-            y_basename = pf.output_name(self.conf, specifiers_list[:3], "responses_")
-            k_folds = exper[i]["k_folds"]
-            new_data_prefix = pf.output_name(
-                self.conf, specifiers_list[:4], "features_",
+            y_basename = pf.output_name(
+                self.conf, specifiers_list[:3], "responses_"
             )
+            k_folds = exper[i]["k_folds"]
 
         # Now call the ensemble script
         output_prefix = pf.output_name(
-            self.conf, self.ensemble_id, "ensemble-preds-"
+            self.conf, self.ensemble_id, "ensemble_model-"
         )
 
         return_code = subprocess.call(
@@ -95,7 +94,6 @@ class Ensemble(luigi.Task):
                 models_basenames,
                 y_basename,
                 str(k_folds),
-                new_data_prefix + "-all.feather",
                 output_prefix + "-all",
                 self.conf.get("paths", "ensemble"),
                 self.ensemble_id
@@ -107,10 +105,10 @@ class Ensemble(luigi.Task):
 
     def output(self):
         output_prefix = pf.output_name(
-            self.conf, self.ensemble_id, "ensemble-preds-"
+            self.conf, self.ensemble_id, "ensemble_model-"
         )
         suffixes = [
-            "-all-cv_trained.feather",
-            "-all-full_trained.feather"
+            "-all-cv_trained.RData",
+            "-all-full_trained.RData"
         ]
         return [luigi.LocalTarget(output_prefix + s) for s in suffixes]
