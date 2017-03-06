ab906b93eb59994ff45564d62af44b174413ee12
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-05 23:44:54 -0800

    Script to join all metrics

---
+++ src/join_evals.R
@@ -0,0 +1,22 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## Join all the feathers in the evaluation directory, run from the src/
+## directory
+
+library("feather")
+f <- list.files(
+  "../data/processed/eval/",
+  pattern = ".feather",
+  full.names = TRUE
+)
+metrics <- lapply(f, read_feather)
+for (i in seq_along(metrics)) {
+  metrics[[i]]$source <- f[i]
+}
+
+write.csv(
+  do.call(rbind, metrics),
+  "../data/processed/eval/all_metrics.csv",
+  row.names = FALSE
+)
