0dd7173be6a851c407f48673fe18b0ed86568406
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-09 10:16:54 -0800

    add person id feature

--- src/utils/feature_funs.R
+++ src/utils/feature_funs.R
@@ -8,9 +8,11 @@ library("plyr")
 library("dplyr")
 library("feather")
 library("phyloseq")
+library("zoo")
+library("caret")
 
 ## ---- functions ----
-feature_fun_generator <- function(f, melted_counts, cv_data, phyloseq_object) {
+feature_fun_generator <- function(f, melted_counts, cv_data, ps) {
   function(test_flag, leave_out_fold = "all-cv", ...) {
     subset_counts <- melted_counts %>%
       left_join(cv_data)
@@ -35,19 +37,26 @@ feature_fun_generator <- function(f, melted_counts, cv_data, phyloseq_object) {
         )
     }
 
-    f(subset_counts, phyloseq_object, ...)
+    f(subset_counts, ps, ...)
   }
 }
 
 #' @examples
-#' cv_cc_relday <- feature_fun_generator(cc_relday, melted_counts, cv_data, phyloseq_object)
+#' cv_cc_relday <- feature_fun_generator(cc_relday, melted_counts, cv_data, ps)
 #' holdout <- cv_cc_relday(FALSE, 1)
-cc_relday <- function(melted_counts, phyloseq_object) {
-  library("zoo")
-  samples <- sample_data(phyloseq_object)
+cc_relday <- function(melted_counts, ps) {
+  samples <- sample_data(ps)
   samples$CC_RelDay <- na.locf(samples$CC_RelDay)
   melted_counts %>%
     left_join(samples) %>%
     select(Meas_ID, rsv, CC_RelDay) %>%
     rename(relative_day = CC_RelDay)
+}
+
+person_id <- function(melted_counts, ps) {
+  model.matrix(
+    ~ subject - 1,
+    data = data.frame(subject = sample_data(ps)$Subject)
+  ) %>%
+    as.matrix()
 }
