835b202300da40e4f4e616371b50def32209160d
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-06 16:47:12 -0700

    Example for building tune grids

--- sandbox/save_model_confs.R
+++ sandbox/save_model_confs.R
@@ -12,3 +12,5 @@ m <- binarize_model(getModelInfo("glmnet", regex <- FALSE)[[1]])
 save(m, file = "conf/glmnet_binarize.RData")
 m <- binarize_model(getModelInfo("xgbTree", regex <- FALSE)[[1]])
 save(m, file = "conf/gbm_binarize.RData")
+
+# getModelInfo("glmnet", FALSE)[[1]]$grid(as.matrix(x_all %>% select(-rsv, -Meas_ID)), as.numeric(y), 10) %>% toJSON
