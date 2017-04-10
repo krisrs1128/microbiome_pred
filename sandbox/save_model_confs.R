b2584309707e53a5a8227021f6c6ff97ec96c5ce
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-10 14:20:07 -0700

    I don't think the labels were saved correctly last time

--- sandbox/save_model_confs.R
+++ sandbox/save_model_confs.R
@@ -1,18 +1,14 @@
 
 source("src/utils/models.R")
 library("caret")
-m <- conditional_positive_model(getModelInfo("rf", regex <- FALSE)[[1]])
-save(m, file = "conf/rf_pos.RData")
-m <- conditional_positive_model(getModelInfo("glmnet", regex <- FALSE)[[1]])
-save(m, file = "conf/glmnet_pos.RData")
-m <- conditional_positive_model(getModelInfo("xgbTree", regex <- FALSE)[[1]])
-save(m, file = "conf/gbm_pos.RData")
+model_base <- c("rf", "gbm", "rpart")
 
-m <- binarize_model(getModelInfo("rf", regex <- FALSE)[[1]])
-save(m, file = "conf/rf_binarize.RData")
-m <- binarize_model(getModelInfo("glmnet", regex <- FALSE)[[1]])
-save(m, file = "conf/glmnet_binarize.RData")
-m <- binarize_model(getModelInfo("xgbTree", regex <- FALSE)[[1]])
-save(m, file = "conf/gbm_binarize.RData")
+for (i in seq_along(model_base)) {
+  m0 <- getModelInfo(model_base[i], regex = FALSE)[[1]]
+  m <- conditional_positive_model(m0)
+  save(m, file = sprintf("conf/%s_pos.RData", model_base[i]))
+  m <- binarize_model(m0)
+  save(m, file = sprintf("conf/%s_binarize.RData", model_base[i]))
+}
 
 # getModelInfo("glmnet", FALSE)[[1]]$grid(as.matrix(x_all %>% select(-rsv, -Meas_ID)), as.numeric(y), 10) %>% toJSON
